361e0618853bf8ae7ea7680d607d47f7e67d1144
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-12-03 20:28:41 +0700

    Fix code outline

--- bin/hydra-docker-image-ecr-deployment-cdk.ts
+++ bin/hydra-docker-image-ecr-deployment-cdk.ts
@@ -16,11 +16,11 @@ const environments = process.env.ENVIRONMENTS?.split(',') ?? ['dev']; // Parsing
 const DEFAULT_IMAGE_VERSION = 'latest';
 
 const envTyped = {
-  POSTGRES_PORT:process.env.POSTGRES_PORT ?? '5432',
-  POSTGRES_USER:process.env.POSTGRES_USER ?? 'postgres',
-  POSTGRES_PASSWORD:process.env.POSTGRES_PASSWORD ?? 'postgres',
-  POSTGRES_BASE_VERSION:process.env.POSTGRES_BASE_VERSION ?? '16.1',
-  POSTGRES_DB_NAME:process.env.POSTGRES_DB_NAME ?? 'hydra',
+  POSTGRES_PORT: process.env.POSTGRES_PORT ?? '5432',
+  POSTGRES_USER: process.env.POSTGRES_USER ?? 'postgres',
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD ?? 'postgres',
+  POSTGRES_BASE_VERSION: process.env.POSTGRES_BASE_VERSION ?? '16.1',
+  POSTGRES_DB_NAME: process.env.POSTGRES_DB_NAME ?? 'hydra',
 }
 
 for (const cdkRegion of cdkRegions) {
